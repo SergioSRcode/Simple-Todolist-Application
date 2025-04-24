e27290e9566aabbe77d2e1b47ce5c2136fd67b39
Sergio Ravera <sergioravera@hotmail.de>
2025-04-24 18:03:10 +0200

    implemented sorting for the todolist

--- todos.js
+++ todos.js
@@ -15,8 +15,36 @@ app.set("view engine", "pug");
 app.use(morgan("common"));
 app.use(express.static("public"));
 
+// Compare todo list titles alphabetically
+const compareByTitle = (todoListA, todoListB) => {
+  let titleA = todoListA.title.toLowerCase();
+  let titleB = todoListB.title.toLowerCase();
+
+  if (titleA < titleB) {
+    return -1;
+  } else if (titleA > titleB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// return the list of todo lists sorted by completion status and title.
+const sortTodoLists = lists => {
+  let undone = lists.filter(todoList => !todoList.isDone());
+  let done   = lists.filter(todoList => todoList.isDone());
+
+  undone.sort(compareByTitle);
+  done.sort(compareByTitle);
+
+  return [].concat(undone, done);
+};
+
+
 app.get("/", (req, res) => {
-  res.render("lists");
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
+  });
 });
 
 // Listener
